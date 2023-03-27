f21e6dd2185d54070cd02c52ca911be3e91d487b
Tarik Amar <tarik.amar@gmail.com>
2023-03-27 18:55:58 +0200

    Follow linter instructions

--- app/controllers/v1/ingredients_controller.rb
+++ app/controllers/v1/ingredients_controller.rb
@@ -1,50 +1,48 @@
-class V1::IngredientsController < ApplicationController
-  def index
-    ingredients = Ingredient.all.order('id DESC')
-    render json: ingredients, status: 200
-  end
+# frozen_string_literal: true
 
-  def show
-    ingredient = Ingredient.find_by(id: params[:id])
-    if ingredient
-      render json: ingredient, status: 200
-    else
-      render json: {error: "Ingredient Not Found."}
+module V1
+  # Handle ingredients
+  class IngredientsController < ApplicationController
+    def index
+      ingredients = Ingredient.all.order('id DESC')
+      render json: ingredients, status: 200
     end
-  end
 
-  def create
-    ingredient = Ingredient.new(
-      name: params[:name],
-      price: params[:price],
-      unit: params[:unit],
-      origin_animal: params[:origin_animal],
-      average_weight: params[:average_weight]
-    )
-
-    if ingredient.save
-      render json: ingredient, status: 201
-    else
-      render json: {error: ingredient.errors}, status: 400
+    def show
+      ingredient = Ingredient.find_by(id: params[:id])
+      if ingredient
+        render json: ingredient, status: 200
+      else
+        render json: { error: 'Ingredient Not Found.' }
+      end
     end
-  end
 
-  def destroy
-    ingredient = Ingredient.find_by(id: params[:id])
+    def create
+      ingredient = Ingredient.new(
+        name: params[:name],
+        price: params[:price],
+        unit: params[:unit],
+        origin_animal: params[:origin_animal],
+        average_weight: params[:average_weight]
+      )
 
-    return render json: { error: 'Ingredient Not Found.' }, status: 404 unless ingredient
+      return render json: ingredient, status: 201 if ingredient.save
 
-    ingredient.delete
-  end
+      render json: { error: ingredient.errors }, status: 400
+    end
 
-  private
+    def destroy
+      ingredient = Ingredient.find_by(id: params[:id])
+
+      return render json: { error: 'Ingredient Not Found.' }, status: 404 unless ingredient
+
+      ingredient.delete
+    end
+
+    private
+
     def ingredient_params
-      params.require(:ingredient).permit([
-        :name,
-        :price,
-        :unit,
-        :origin_animal,
-        :average_weight,
-      ])
+      params.require(:ingredient).permit(%i[name price unit origin_animal average_weight])
     end
+  end
 end
