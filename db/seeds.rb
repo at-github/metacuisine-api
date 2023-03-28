bac26198c5d8ecf14c2266465a5a50fd97bb6a7f
Tarik Amar <tarik.amar@gmail.com>
2023-03-28 14:28:04 +0200

    Ingredient - Seed available

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+Ingredient.destroy_all
+
+Ingredient.create!(
+  [
+    {
+      name: 'Farine T65',
+      price: 1.29,
+      unit: 'kg'
+    },
+    {
+      name: 'Sel fin',
+      price: 1.95,
+      unit: 'kg'
+    },
+    {
+      name: 'Levure sèche',
+      price: 37.75,
+      unit: 'kg'
+    },
+    {
+      name: 'Sucre semoule',
+      price: 2.57,
+      unit: 'kg'
+    },
+    {
+      name: 'Huile d’olives Grecque',
+      price: 9.97,
+      unit: 'kg'
+    }
+  ]
+)
+
+puts "Created #{Ingredient.count} ingredients"
