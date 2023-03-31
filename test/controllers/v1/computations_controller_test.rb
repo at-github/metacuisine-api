46acdcd3d5360c2ddea6b751632d12651b8bca08
Tarik Amar <tarik.amar@gmail.com>
2023-03-31 19:01:17 +0200

    Computation - Start to test controller

--- test/controllers/v1/computations_controller_test.rb
+++ test/controllers/v1/computations_controller_test.rb
@@ -1,7 +1,35 @@
-require "test_helper"
+# frozen_string_literal: true
 
-class V1::ComputationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+require 'test_helper'
+
+module V1
+  class ComputationsControllerTest < ActionDispatch::IntegrationTest
+    test 'should get index' do
+      get v1_computations_url
+      assert_response :success
+
+      computations = JSON.parse @response.body
+      assert_operator 0, :<, computations.count
+
+      computations.each do |computation|
+        assert computation.key? 'title'
+        assert computation.key? 'ingredients'
+      end
+    end
+
+    test 'should get show' do
+      get v1_computation_url(Computation.find_by(title: 'Chiffrage avec des ingrédients'))
+      assert_response :success
+
+      computation = JSON.parse @response.body
+      assert computation.key? 'title'
+      assert computation.key? 'ingredients'
+
+      computation['ingredients'].each do |ingredient|
+        assert ingredient.key? 'name'
+        assert ingredient.key? 'price'
+        assert ingredient.key? 'unit'
+      end
+    end
+  end
 end
