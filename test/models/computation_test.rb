0a57a7696de8aa8e8ff6502de6678c07e93abc59
Tarik Amar <tarik.amar@gmail.com>
2023-03-31 17:26:48 +0200

    Computation - Test title uniqueness

--- test/models/computation_test.rb
+++ test/models/computation_test.rb
@@ -7,4 +7,15 @@ class ComputationTest < ActiveSupport::TestCase
     computation = Computation.new
     assert_not computation.save, 'Saved computation without title'
   end
+
+  test 'should not save computation with duplicate title' do
+    computation_one = Computation.new({ title: 'title' })
+    assert computation_one.save
+
+    computation_two = Computation.new({ title: 'title' })
+    assert_not computation_two.save, 'Saved computation with title already exist'
+
+    computation_three = Computation.new({ title: 'Title' })
+    assert_not computation_three.save, 'Saved computation with title already exist but different case'
+  end
 end
